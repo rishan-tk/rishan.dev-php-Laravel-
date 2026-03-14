15a2386d386896ccf547d33a8d8461072d3c2456
rishan-tk <rishan-tk@rishan.dev>
2026-03-14 06:37:20 +0000

    favicon update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <meta name="theme-color" content="#0d1117">
 
     {{-- Favicon --}}
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=2">
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg?v=3">
 
     {{-- Prevent flash of wrong theme --}}
     <script>
